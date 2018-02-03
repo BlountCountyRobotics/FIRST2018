ba052ca77306aea0204f3210feddd19727df07bc
porgull <connorlbark@gmail.com>
2018-02-03 17:09:04 -0500

    Init subsystems

---
+++ src/org/usfirst/frc/team4504/robot/subsystems/Elevator.java
@@ -0,0 +1,17 @@
+package org.usfirst.frc.team4504.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class Elevator extends Subsystem {
+
+	
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+}
+
