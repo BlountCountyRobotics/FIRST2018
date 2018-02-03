9cd822f6103a5bffd42eddd97d349379ba9e74fe
porgull <connorlbark@gmail.com>
2018-02-03 18:15:10 -0500

    Init OI

--- src/org/usfirst/frc/team4504/robot/OI.java
+++ src/org/usfirst/frc/team4504/robot/OI.java
@@ -7,36 +7,57 @@
 
 package org.usfirst.frc.team4504.robot;
 
+import org.usfirst.frc.team4504.robot.objects.ButtonBoard;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
-	//// CREATING BUTTONS
-	// One type of button is a joystick button which is any button on a
-	//// joystick.
-	// You create one by telling it which joystick it's on and which button
-	// number it is.
-	// Joystick stick = new Joystick(port);
-	// Button button = new JoystickButton(stick, buttonNumber);
+	
+	// Controllers
+	private XboxController controller;
+	private ButtonBoard buttonBoard;
+	
+	// Buttons
+	private JoystickButton controlGrabber; // button; press
+	private JoystickButton climb; // button; hold (depends on implementation; may need two buttons)
+	private JoystickButton shoot; // button; hold
+	private JoystickButton shootInverted; // button; hold
+	private JoystickButton elevatorUp; // button; hold
+	private JoystickButton elevatorDown; // button; hold
+	
+	private JoystickButton changeShooterSpeed; // switch
+	// this makes one of the dials control the shooter speed
+	// instead of a set speed
+	
+	public OI()
+	{
+		controller = new XboxController(0);
+		buttonBoard =  new ButtonBoard(1);
+		
+		controlGrabber = buttonBoard.getBottomLeft();
+		climb = buttonBoard.getTopLeft();
+		shoot = buttonBoard.getBottomMiddle();
+		shootInverted = buttonBoard.getBottomRight();
+		elevatorUp = buttonBoard.getTopMiddle();
+		elevatorDown = buttonBoard.getTopRight();
+		changeShooterSpeed = buttonBoard.getTopSwitch();
+	}
+	
 
-	// There are a few additional built in buttons you can use. Additionally,
-	// by subclassing Button you can create custom triggers and bind those to
-	// commands the same as any other Button.
-
-	//// TRIGGERING COMMANDS WITH BUTTONS
-	// Once you have a button, it's trivial to bind it to a button in one of
-	// three ways:
-
-	// Start the command when the button is pressed and let it run the command
-	// until it is finished as determined by it's isFinished method.
-	// button.whenPressed(new ExampleCommand());
-
-	// Run the command while the button is being held down and interrupt it once
-	// the button is released.
-	// button.whileHeld(new ExampleCommand());
-
-	// Start the command when the button is released and let it run the command
-	// until it is finished as determined by it's isFinished method.
-	// button.whenReleased(new ExampleCommand());
+	
+	public Joystick getButtonBoard()
+	{
+		return buttonBoard;
+	}
+	
+	public XboxController getController()
+	{
+		return controller;
+	}
 }
