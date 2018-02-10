127d400ef51de437d15ac55fc873d948f9600a50
porgull <connorlbark@gmail.com>
2018-02-10 16:09:11 -0500

    Implement shooter

--- src/org/usfirst/frc/team4504/robot/subsystems/Shooter.java
+++ src/org/usfirst/frc/team4504/robot/subsystems/Shooter.java
@@ -1,5 +1,11 @@
 package org.usfirst.frc.team4504.robot.subsystems;
 
+import org.usfirst.frc.team4504.robot.RobotMap;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
@@ -10,9 +16,47 @@ public class Shooter extends Subsystem {
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 
+	TalonSRX left;
+	TalonSRX right;
+	
+	public void shoot()
+	{
+		left.set(ControlMode.Velocity, 
+				RobotMap.Shooter.shootingRPM * RobotMap.Constants.rpmConversion);
+		right.set(ControlMode.Velocity, 
+				RobotMap.Shooter.shootingRPM * RobotMap.Constants.rpmConversion);
+	}
+	
+	public void shoot(double percent)
+	{
+		left.set(ControlMode.Velocity, 
+				percent * RobotMap.Shooter.maxRPM * RobotMap.Constants.rpmConversion);
+		right.set(ControlMode.Velocity, 
+				percent * RobotMap.Shooter.maxRPM * RobotMap.Constants.rpmConversion);
+	}
+	
+	public void intake()
+	{
+		left.set(ControlMode.Velocity, 
+				RobotMap.Shooter.intakeRPM * RobotMap.Constants.rpmConversion);
+		right.set(ControlMode.Velocity, 
+				RobotMap.Shooter.intakeRPM * RobotMap.Constants.rpmConversion);
+	}
+	
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
+    }
+    
+    public Shooter()
+    {
+    	left  = new TalonSRX(RobotMap.Shooter.left);
+    	right = new TalonSRX(RobotMap.Shooter.right);
+    	
+    	left.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative,
+    			0, RobotMap.Constants.timeout);
+    	right.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative,
+    			0, RobotMap.Constants.timeout);
     }
 }
 
