f95b164aabc00023ade726f8480b988c8b8d48e5
porgull <connorlbark@gmail.com>
2018-02-13 18:29:27 -0500

    Add the rest of the commands

--- src/org/usfirst/frc/team4504/robot/commands/ConstantElevatorSt1.java
+++ src/org/usfirst/frc/team4504/robot/commands/ConstantElevatorSt1.java
@@ -5,9 +5,9 @@ import edu.wpi.first.wpilibj.command.Command;
 /**
  *
  */
-public class ConstantElevator extends Command {
+public class ConstantElevatorSt1 extends Command {
 
-    public ConstantElevator() {
+    public ConstantElevatorSt1() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
     }
