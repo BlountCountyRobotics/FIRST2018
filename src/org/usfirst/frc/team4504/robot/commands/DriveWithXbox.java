5d0264286880aecdc9a68f0dd397df0a854ecf89
porgull <connorlbark@gmail.com>
2018-02-18 13:28:24 -0500

    Remove accidental indent

--- src/org/usfirst/frc/team4504/robot/commands/DriveWithXbox.java
+++ src/org/usfirst/frc/team4504/robot/commands/DriveWithXbox.java
@@ -23,7 +23,7 @@ public class DriveWithXbox extends Command {
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
     	BCRXboxController controller = Robot.oi.getController();
-    	 Robot.driveTrain.driveWithXboxRPM(controller);
+    	Robot.driveTrain.driveWithXboxRPM(controller);
     }
 
     // Make this return true when this Command no longer needs to run execute()
