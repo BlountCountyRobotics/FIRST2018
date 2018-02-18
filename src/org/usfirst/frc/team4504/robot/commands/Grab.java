e369af2c246c99712000159f0bf0199656f1976f
porgull <connorlbark@gmail.com>
2018-02-18 14:51:53 -0500

    Swap grab() from init to exec

--- src/org/usfirst/frc/team4504/robot/commands/Grab.java
+++ src/org/usfirst/frc/team4504/robot/commands/Grab.java
@@ -17,12 +17,11 @@ public class Grab extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
-    	Robot.grabber.grab();
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	
+    	Robot.grabber.grab();
     }
 
     // Make this return true when this Command no longer needs to run execute()
